53f0b54e826ea0f0d6b9dad4590cdf559333820a
Mark Daggett <mark@humansized.com>
2014-11-27 11:24:55 -0600

    Update d4 to the latest version.

--- src/assets/js/d4.js
+++ src/assets/js/d4.js
@@ -1,6 +1,6 @@
-/*! d4 - v0.8.10
+/*! d4 - v0.8.16
  *  License: MIT Expat
- *  Date: 2014-10-17
+ *  Date: 2014-11-27
  *  Copyright: Mark Daggett, D4 Team
  */
 /*!
@@ -326,12 +326,9 @@
   };
 
   var prepareDataForFeature = function(opts, name, data) {
-    var feature = opts.features[name];
-    if (d4.isFunction(feature.prepare)) {
-      data = feature.prepare.bind(opts)(data);
-      if (d4.isUndefined(data)) {
-        err('"feature.prepare()" must return a data array. However, the prepare function for the "{0}" feature did not', name);
-      }
+    var result = opts.features[name].accessors.beforeRender.bind(opts)(data);
+    if (d4.isDefined(result)) {
+      data = result;
     }
     return data;
   };
@@ -357,16 +354,16 @@
   };
 
   var scaffoldChart = function(selection) {
-    if (selection.tagName == "svg") {
+    if (selection.tagName === 'svg') {
       this.container = d3.select(selection)
-        .classed("d4", true)
-        .classed("chart", true)
+        .classed('d4', true)
+        .classed('chart', true)
         .attr('width', Math.max(0, this.width + this.margin.left + this.margin.right))
         .attr('height', Math.max(0, this.height + this.margin.top + this.margin.bottom));
-    } else if (selection.tagName == "g") {
+    } else if (selection.tagName === 'g') {
       this.container = d3.select(selection)
-        .classed("d4", true)
-        .classed("chart", true)
+        .classed('d4', true)
+        .classed('chart', true);
 
     } else {
       this.container = d4.appendOnce(d3.select(selection), 'svg.d4.chart')
@@ -482,6 +479,7 @@
       var baseFeature = {
         accessors: {
           afterRender: function() {},
+          beforeRender: function() {}
         },
         proxies: []
       };
@@ -2076,13 +2074,13 @@
 
       return {
         accessors: {
+          beforeRender: function(data) {
+            return calculateStackTotals.bind(this)(data);
+          },
           y: function(d) {
             var padding = 5;
             return this.y(d.size) - padding;
           }
-        },
-        prepare: function(data) {
-          return calculateStackTotals.bind(this)(data);
         }
       };
     };
@@ -2220,14 +2218,15 @@
 
       return {
         accessors: {
+          beforeRender: function(data) {
+            return calculateStackTotals.bind(this)(data);
+          },
+
           x: function(d) {
             var padding = 5;
             return this.x(d.size) + padding;
           }
         },
-        prepare: function(data) {
-          return calculateStackTotals.bind(this)(data);
-        }
       };
     };
 
@@ -4004,6 +4003,13 @@
   d4.feature('waterfallConnectors', function(name) {
     return {
       accessors: {
+        beforeRender: function(data) {
+          var d = data.map(function(o) {
+            return o.values[0];
+          });
+          return d4.flatten(d);
+        },
+
         classes: function(d, i) {
           return 'series' + i;
         },
@@ -4036,12 +4042,6 @@
             return this.y(d[this.y.$key]);
           }
         }
-      },
-      prepare: function(data) {
-        var d = data.map(function(o) {
-          return o.values[0];
-        });
-        return d4.flatten(d);
       },
 
       render: function(scope, data, selection) {
